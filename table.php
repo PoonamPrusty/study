4526ef9f727298177529cab23ad19a0a9e392218
Poonam Prusty <poonamprusty20@gmail.com>
2011-12-08 22:09:29 +0530

    tables

--- table.php
+++ table.php
@@ -1,5 +1,11 @@
 <?php
-		$db=new PDO('mysql:databasename=check;host=localhost;charset=utf-8;','root','');}
-		$sql="INSERT INTO `check`_`try`(`ID`,`NAME`)VALUES(\'1\',\abc'\');";
-		$sql="INSERT INTO `check`_`try`(`NAME`)VALUES(\'def\');";
+        $dbh = new PDO('mysql:dbname=guestbook;host=localhost;charset=utf-8;','root','');
+    
+	$sql="INSERT INTO `check`_`try`(`ID`,`NAME`)VALUES(\'1\',\abc'\');";
+	$dbh->prepare($sql)->execute();
+	$v="SELECT * FROM `try`";
+	$dbh->prepare($v)->execute();
+	$var="INSERT INTO `check`_`try`(`NAME`)VALUES(\'def\');";
+	$dbh->prepare($var)->execute();
+	
 	
